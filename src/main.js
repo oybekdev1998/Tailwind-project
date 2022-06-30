b0b63267f88b7732f80676901359e1a8f49c23ab
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-06-30 13:35:39 +0500

    Added dark mode

--- src/main.js
+++ src/main.js
@@ -10,3 +10,22 @@ function burgerMenu() {
 }
 
 burgerMenu()
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark')
+} else {
+  document.documentElement.classList.remove('dark')
+}
+
+const modeBtn = document.querySelector('#mode')
+modeBtn.addEventListener('click', () => {
+  document.querySelector('html').classList.toggle('dark')
+
+  if(document.querySelector('html').classList.contains('dark')) {
+    localStorage.theme = 'dark'
+  }
+  else {
+    localStorage.theme = 'light'
+  }
+})
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**.html"],
+  darkMode: 'class',
   theme: {
     screens: {
       'lg': {'max': '992px'},
